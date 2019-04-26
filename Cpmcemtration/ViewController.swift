9bf4c89b638f067d93b62c28349bb0bbd2df9143
x1700209131 <775812130@qq.com>
2019-04-26 10:00:57 +0800

    运用Swift，制作Concentration简单小游戏

--- Cpmcemtration/ViewController.swift
+++ Cpmcemtration/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
-
-
+    var emojiChoices = ["🎃","👻","🎃","👻"]
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let carNumber = cardButtons.firstIndex(of: sender) {
+            flipCard(withEmoji: emojiChoices[carNumber], on: sender)
+        } else {
+            print("chosen card was not in cardButtons")
+        }
+       
+    }
+    
+  
+   
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    func flipCard(withEmoji emoji:String, on button:UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor  = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor  = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
 }
 
