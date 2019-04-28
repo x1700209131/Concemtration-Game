12979b3fc6d1d35de18c61ab6888782e50a2025c
x1700209131 <775812130@qq.com>
2019-04-28 16:09:59 +0800

    更新——更多

---
+++ Cpmcemtration/Concentration.swift
@@ -0,0 +1,45 @@
+//
+//  Concentration.swift
+//  Cpmcemtration
+//
+//  Created by cc on 2019/4/28.
+//  Copyright © 2019 cc. All rights reserved.
+//
+
+import Foundation
+
+class Concentration{
+    
+    var cards = [Card]()
+    
+    var indexOfOneAndOnlyFaceUpCard: Int?
+    
+    func chooseCard(at index: Int)  {
+        if !cards[index].IsMatched {
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
+                // 匹配成功
+                if cards[matchIndex].identifier == cards[index].identifier{
+                    cards[matchIndex].IsMatched = true
+                    cards[index].IsMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = nil
+            } else {
+                //没有卡片朝上或者两张不匹配的卡片朝上
+                for filpDownIndex in cards.indices {
+                    cards[filpDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFaceUpCard = index
+            }
+        }
+    }
+    
+    init(numberOfPairsOfCard: Int) {
+        for _ in 1...numberOfPairsOfCard {
+            let card = Card()
+            cards += [card, card]
+            
+        }
+    }
+}
--- Cpmcemtration/ViewController.swift
+++ Cpmcemtration/ViewController.swift
@@ -17,32 +17,47 @@ class ViewController: UIViewController {
             flipCountLabel.text = "Flips: \(flipCount)"
         }
     }
-    var emojiChoices = ["🎃","👻","🎃","👻"]
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+
     @IBOutlet var cardButtons: [UIButton]!
     
     @IBAction func touchCard(_ sender: UIButton) {
         flipCount += 1
         if let carNumber = cardButtons.firstIndex(of: sender) {
-            flipCard(withEmoji: emojiChoices[carNumber], on: sender)
+            game.chooseCard(at: carNumber)
+            updateViewFromModel()
         } else {
             print("chosen card was not in cardButtons")
         }
        
     }
     
-  
-   
-    
-    @IBOutlet weak var flipCountLabel: UILabel!
-    
-    func flipCard(withEmoji emoji:String, on button:UIButton){
-        if button.currentTitle == emoji {
-            button.setTitle("", for: UIControl.State.normal)
-            button.backgroundColor  = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
-        } else {
-            button.setTitle(emoji, for: UIControl.State.normal)
-            button.backgroundColor  = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    func updateViewFromModel()  {
+        for index in cardButtons.indices {
+            let button = cardButtons[index]
+            let card = game.cards[index]
+            if card.isFaceUp{
+                button.setTitle(emoji(for: card), for: UIControl.State.normal)
+                button.backgroundColor  = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+            } else {
+                button.setTitle("", for: UIControl.State.normal)
+                button.backgroundColor  = card.IsMatched ? #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+            }
         }
+    }
+
+    
+    var emojiChoices = ["🦇", "😱", "🙀", "😈", "🎃", "👻", "🍭", "🍬", "🍎"]
+    
+    var emoji =  Dictionary<Int,String>() //字典的特殊语法糖  var emoji = [INt:String]()
+
+    func emoji(for card: Card) -> String {
+        if emoji[card.identifier] == nil , emojiChoices.count > 0{
+                let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
+                emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+            }
+        return emoji[card.identifier] ?? "?"
     }
 }
 
